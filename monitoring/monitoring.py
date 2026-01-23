31581ede23e67255541a308d51fe8eb664eddd6f
Riccardo Ciullini <140012831+WebDevWizz@users.noreply.github.com>
2026-01-23 17:25:20 +0000

    implementazione sistema monitoraggio completo

--- monitoring/monitoring.py
+++ monitoring/monitoring.py
@@ -1,37 +1,89 @@
 import csv
-import os
+import os 
+from collections import Counter 
 from datetime import datetime
 from src.inference import predict_sentiment
 
 
-METRICS_FILE = "metrics/sentiment_metrics.csv"
+# ⚠️ Dopo il feedback dato dal professore post prima consegna (e dopo varie prove fatte nei giorni scorsi), 
+# ho deciso di scrivere un'unica funzione completa di monitoraggio che non si limita solo a monitorare la confidence, ma che calcoli anche la distribuzione di ogni label 
+# (= % di label positive/neutrali/negative) e soprattutto il Drift (rispetto ad una baseline, giusto per avere un punto di riferimento da cui partire nel monitoraggio; tra l'altro, dopo vari tentativi, ho deciso di impostare la soglia del drift a 0.2)
 
-# Questo è il semplice monitoraggio che implemento:
-def monitor_text(text):
-    # Raccolgo gli scores: 
-    scores = predict_sentiment(text)
 
-    # Restituendo un dizionario di prob, prendo quella più alta: 
-    sentiment = max(scores, key = scores.get)
+def monitor_batch(texts, csv_path, baseline_distribution = None, drift_threshold = 0.2): 
+    # Definisco 2 vettori in cui salvare, rispettivamente, i sentiment (= label) predetti e la confidence: 
+    sentiments = []
+    confidences = []
 
-    # ⚠️ Correzione post feedback del professore -> come richiesto, salvo anche la confidence della predizione: 
-    confidence = scores[sentiment]
+    # Flag che mi serve in inferenza per controllare se il file csv già esiste: 
+    file_exists = os.path.isfile(csv_path)
 
-    # Controllo se il file esiste già: 
-    file_exists = os.path.isfile(METRICS_FILE)
-
-    # Esporto nel file csv: 
-    with open(METRICS_FILE, "a", newline= "") as f: 
+    # 1. INFERENZA -> Inizio a calcolare le predizioni (e a prendere quella più grande) e le confidence per ogni text, e le scrivo nel file csv
+    with open(csv_path, mode="a", newline="") as f: 
         writer = csv.writer(f)
 
-        # Se il file è nuovo, scrivo l'header prima dei dati
+        # Check che il file non esista già
         if not file_exists:
+            # Scrivo l'header di ogni colonna: 
             writer.writerow(["timestamp", "sentiment", "confidence"])
 
-        writer.writerow([datetime.now().isoformat(), sentiment, confidence])
 
-    return sentiment, confidence
+        # A questo punto faccio inferenza: 
+        for text in texts: 
+            predictions = predict_sentiment(text)
+            label = max(predictions, key = predictions.get).lower() # Devo metterle in minuscolo, in quanto il modello fornito nella traccia ha label tutte in minuscolo
+            # E ora calcolo la confidence (= probabilità della predizione)
+            confidence = predictions[label]
+
+            # Calcolo anche il timestamp: 
+            timestamp = datetime.utcnow().isoformat()
+
+            # Scrivo nel file: 
+            writer.writerow([timestamp, label, confidence])
+
+            # Salvo le label e le confidence negli appositi vettori: 
+            sentiments.append(label)
+            confidences.append(confidence)
+
+    
+    # 2. Calcolo Distribuzione -> Uso un Counter per raccogliere in un dict le occorrenze di ogni label, per poi dividere per il numero totale di label e ricavare le distribuzioni: 
+    counts = Counter(sentiments)
+    dim = len(sentiments)
+
+    # Costruisco il mio dizionario delle distribuzioni (OSS: con counts.get("Positive") raccolgo tutte le occorrenze con label "Positive", ecc.): 
+    distribution = {
+        "positive": counts.get("positive", 0) / dim,
+        "neutral": counts.get("neutral", 0) / dim,
+        "negative": counts.get("negative", 0) / dim
+    }
+
+    # Per completezza, calcolo anche la confidence media: 
+    avg_confidence = sum(confidences)/dim
 
 
-# ⚠️ Correzione post feedback del professore -> ho deciso di implementare anche uno script per testare la funzione, piuttosto che fare tutto da terminale.
-# La funzione in questione la può trovare sempre in questa cartella
+    # 3. Drift -> si verifica quando le caratteristiche dei dati in ingresso cambiano nel tempo; come detto inizialmente, il cambiamento lo monitoro utilizzando una soglia:
+    is_drift = False 
+    if baseline_distribution: 
+        for label in distribution: 
+            # Calcolo del drift: (ex: se normalmente abbiamo una distribuzione di "Positive" dell'80%, ma in seguito ad un bug questa si abbassa al 30%, la differenza sarà: |0.3 - 0.8| = 0.5 > 0.2 => DRIFT)
+            if abs(distribution[label] - baseline_distribution.get(label, 0)) > drift_threshold: 
+                is_drift = True # A questo punto, il sistema avrebbe poi bisogno di un retraining
+                break
+
+    
+    # 4. Stampo gli output del monitoraggio: 
+    print("\nBatch timestamp:", datetime.utcnow().isoformat())
+    print("\nDistributions:")
+    print(f"\nPositive: {distribution['positive']:.0%}")
+    print(f"\nNeutral: {distribution['neutral']:.0%}")
+    print(f"\nNegative: {distribution['negative']:.0%}")
+    print(f"\nAvg confidence: {avg_confidence:.2f}")
+    print(f"Drift detected:", "YES" if is_drift else "NO")
+
+
+    # La funzione mi ritorna poi un dict contenente distribuzione, confidence media e drift: 
+    return {
+        "distribution": distribution,
+        "avg confidence": avg_confidence,
+        "drift": is_drift
+    }
--- monitoring/monitoring_test.py
+++ monitoring/monitoring_test.py
@@ -1,22 +1,46 @@
-# In questo file ho scritto questo script per testare la funzione e il file csv più facilmente, rispetto a prima del feedback dove invece era necessario passare per il terminale
+"""
+Script di test per il sistema di monitoraggio.
 
-from monitoring.monitoring import monitor_text
+Permette di validare facilmente:
+- il corretto funzionamento della funzione di monitoring
+- la scrittura dei risultati nel file CSV
+- il calcolo di sentiment e confidence
+
+Rispetto all'utilizzo diretto da terminale, questo script
+fornisce un test controllato e ripetibile.
+"""
+
+from monitoring.monitoring import monitor_batch
 
 def run_test():
-    print("Inizio Monitoraggio...\n")
+    print("Start Monitoring...\n")
 
-    frasi_test = [
-        "Questa azienda è fantastica, la adoro", 
-        "Il prodotto di questa azienda è davvero pessimo", 
-        "Pacco arrivato in orario, tutto ok", 
-        "Pensavo meglio"
+    test_sentences = [
+        "This company is amazing, I really love it",
+        "The product is terrible and disappointing",
+        "Package arrived on time, everything is fine",
+        "I expected something better",
+        "What a terrible experience",
+        "This company sucks!",
+        "Not bad"
     ]
 
-    for test in frasi_test: 
-        sentiment, confidence = monitor_text(test)
-        print(f"Testo: {test} -> sentiment: {sentiment}, confidence: {confidence:.2f}")
+    # Baseline (opzionale, può anche essere rimossa): 
+    baseline = {
+        "Positive": 0.7, 
+        "Neutral": 0.2, 
+        "Negative": 0.1
+    }
 
-    print("\nTest completato!")
+    results = monitor_batch(
+        texts=test_sentences, 
+        csv_path="metrics/sentiment_metrics.csv", 
+        baseline_distribution=baseline, 
+        drift_threshold=0.2
+    )
+
+    print("End Monitoring")
+    
 
 
 if __name__ == "__main__":
